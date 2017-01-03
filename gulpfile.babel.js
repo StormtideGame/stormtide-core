6705eafad3c95ceb235fe6491681fac197b5bf62
Lucien Greathouse <me@lpghatguy.com>
2017-01-02 21:47:44 -0700

    Update gulpfile to compile JSON and fix up watching

---
+++ gulpfile.babel.js
@@ -0,0 +1,37 @@
+// @flow
+
+"use strict";
+
+import gulp from "gulp";
+
+import plumber from "gulp-plumber";
+import babel from "gulp-babel";
+import sourcemaps from "gulp-sourcemaps";
+import merge from "merge-stream";
+
+const build: Object = {};
+
+build.lib = (files = "src/**/*.js") => {
+	return gulp.src(files, { base: "src" })
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(sourcemaps.write("."))
+		.pipe(gulp.dest("lib"));
+};
+
+build.json = (files = "src/**/*.json") => {
+	return gulp.src(files, { base: "src" })
+		.pipe(gulp.dest("lib"));
+};
+
+build.watch = () => {
+	gulp.watch("src/**/*.js", change => build.lib(change.path));
+	gulp.watch("src/**/*.json", change => build.json(change.path));
+};
+
+build.default = () => merge([build.lib(), build.json()]);
+
+for (const key of Object.keys(build)) {
+	gulp.task(key, build[key]);
+}
